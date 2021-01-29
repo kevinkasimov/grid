3491d4eb9e6187872300a2ffcf3129e22d0da4de
kevinkasimov <51176791+kevinkasimov@users.noreply.github.com>
2021-01-28 22:55:08 -0800

    Add files via upload

---
+++ draw_grid.py
@@ -0,0 +1,17 @@
+# 3.14 Exercises
+# Exercise 3.3
+
+def draw_grid():
+    """
+    draws a grid with two rows and two columns
+    """
+    for i in range(0,2):
+        print('+',('- '*4)[:-1],'+', ('- '*4)[:-1], '+')
+        for i in range(0,5):
+            print('|', ' '*7, '|', ' '*7, '|')
+        #end for
+    #end for
+    print('+',('- '*4)[:-1],'+', ('- '*4)[:-1], '+')
+
+    
+    return None
